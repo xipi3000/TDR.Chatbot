398bed09fd900775685a1f04d0b9a71a410596f9
xipi3000 <58340151+xipi3000@users.noreply.github.com>
2019-11-29 18:05:35 +0100

    Add files via upload

---
+++ server C#/server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using WindowsInput;
+using WindowsInput.Native;
+
+public class UdpSrvrSample
+{
+    public static void Main()
+    {
+        InputSimulator sim = new InputSimulator();
+        UdpClient udpServer = new UdpClient(60668);
+
+        while (true)
+        {
+            var remoteEP = new IPEndPoint(IPAddress.Any, 60668);
+            var data = udpServer.Receive(ref remoteEP); // listen on port 11000
+            Console.Write("receive data from " + remoteEP.ToString());
+            if (remoteEP.ToString() == "192.168.1.39:60668")
+            {
+
+
+                // Press 0 key
+                //sim.Keyboard.KeyPress(VirtualKeyCode.VK_G);
+
+            }
+        }
+    }
+}
